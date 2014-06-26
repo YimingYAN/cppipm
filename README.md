c313e73658162ebdd3e9ec5759d227f7e056392b
Yiming Yan <yanyiming1986@gmail.com>
2014-06-27 00:30:17 +0100

    First commit. Created an Xcode project for cppipm.

---
+++ README.md
@@ -0,0 +1,8 @@
+cppipm
+=====
+
+C Plus Plus implementation of the Interior Point Method (CPIPM)
+
+
+
+In the aim to learn C+++
---
+++ src/test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main (int argc, char* argv[])
+{
+	using namespace std;
+	cout<< "Test file for classes" << endl;
+
+	return 0;
+}
