7b84d9cf88d7f307da8e9f598631b69411c8b766
Yiming Yan <yanyiming1986@gmail.com>
2014-07-11 13:35:40 +0100

    Started new class: mpsReader for reading problems in the MPS format.

---
+++ src/mpsReader.cpp
@@ -0,0 +1,9 @@
+//
+//  mpsReader.cpp
+//  cppipm
+//
+//  Created by Yiming Yan on 11/07/2014.
+//  Copyright (c) 2014 Yiming. All rights reserved.
+//
+
+#include "mpsReader.h"
---
+++ src/mpsReader.h
@@ -0,0 +1,24 @@
+//
+//  mpsReader.h
+//  cppipm
+//
+//  Created by Yiming Yan on 11/07/2014.
+//  Copyright (c) 2014 Yiming. All rights reserved.
+//
+
+#ifndef cppipm_mpsReader_h
+#define cppipm_mpsReader_h
+
+#include<iostream>
+
+class mpsReader
+{
+
+public:
+    mpsReader(std::string fileName);
+private:
+    
+};
+
+
+#endif
