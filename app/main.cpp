5f11d7171409471c70a27de5c507ae16d8f6058c
Yiming Yan <yiming.yan@sheffield.ac.uk>
2016-03-07 23:08:52 +0000

    find a minor bug in main.cpp

--- app/main.cpp
+++ app/main.cpp
@@ -4,12 +4,12 @@ using std::cout;
 using std::endl;
 int main (int argc, char *argv[])
 {
-    if(argc < 2) {
+    if(argc < 3) {
         cout << "Usage: executable <algorithm_name> <problem_file>" << endl;
         return 0;
     }
     cout << "Running the app."       << endl;
-    cout << "Algorithm: " << argv[0] << endl;
-    cout << "Problem  : " << argv[1] << endl;
+    cout << "Algorithm: " << argv[1] << endl;
+    cout << "Problem  : " << argv[2] << endl;
     return 0;
 }
